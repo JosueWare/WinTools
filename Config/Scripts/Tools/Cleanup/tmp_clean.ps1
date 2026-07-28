fc770ecac1022f04a806b4b74695157d68575650
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-07-28 19:05:40 -0300

    [refactor] Trabalhando no questionário..

--- Config/Scripts/Tools/Cleanup/tmp_clean.ps1
+++ Config/Scripts/Tools/Cleanup/tmp_clean.ps1
@@ -4,7 +4,7 @@
     $folderAppData_Local = [System.Environment]::GetFolderPath('LocalApplicationData')
     $folderWindows = [System.Environment]::GetFolderPath('Windows')
 
-# Cleanup Process
+# Cleanup Process (GUI)
 Clear-Host
 
     Start-Sleep -Seconds 1
@@ -14,3 +14,23 @@ Write-Host "    Deseja limpar os arquivos agora?" -NoNewline
     Start-Sleep -Milliseconds 500
 Write-Host "    (Dir: 'Local\Temp', 'Windows\Temp', 'Windows\Prefetch')" -ForegroundColor DarkGray
     Write-Host "" <##>
+    Start-Sleep -Milliseconds 500
+    Write-Host "" <##>
+Write-Host "        [S] Sim / [N] Não"
+    Write-Host "" <##>
+
+$questStartCleanupProcess = Read-Host
+
+    switch ($questStartCleanupProcess) {
+
+        "S" {}
+        "N" {}
+
+        Default {
+                Write-Host "" <##>
+            Write-Host "    Resposta inválida" -ForegroundColor Red
+                Write-Host "" <##>
+
+            Set-Location "$HOME"
+        }
+    }

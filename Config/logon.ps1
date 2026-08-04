289fa427bbba08b1490684ff111137900815e523
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-08-03 23:17:06 -0300

    [cleanup]

--- Config/logon.ps1
+++ Config/logon.ps1
@@ -5,15 +5,9 @@
     $dep_file = Resolve-Path ".\Config\Navigation\Help\depedences.ps1"
 
 # Init
-
-    <# PowerShell #>
-
-        if (Get-Command "pwsh.exe" -ErrorAction SilentlyContinue) {
-
-            Start-Process -FilePath "pwsh.exe" -ArgumentList "-NoProfile", "-NoExit", "-ExecutionPolicy Bypass", "-Command $main_file" -Verb RunAs
-        }
-
-            else {
-
-                Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-NoExit", "-ExecutionPolicy Bypass", "-Command $dep_file" -Verb RunAs
-            }
+if (Get-Command "pwsh.exe" -ErrorAction SilentlyContinue) {
+    Start-Process -FilePath "pwsh.exe" -ArgumentList "-NoProfile", "-NoExit", "-ExecutionPolicy Bypass", "-Command $main_file" -Verb RunAs
+}
+    else {
+            Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-NoExit", "-ExecutionPolicy Bypass", "-Command $dep_file" -Verb RunAs
+    }

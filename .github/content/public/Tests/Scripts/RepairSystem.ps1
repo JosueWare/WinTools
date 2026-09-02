37ada76ec6a0e45a0633b7aec50a6cfb11b07b97
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-09-02 03:43:16 -0300

    working: Estou trabalhando no script

---
+++ .github/content/public/Tests/Scripts/RepairSystem.ps1
@@ -0,0 +1,39 @@
+# ENV
+[bool]$Internet_NET = Test-Connection -TargetName "8.8.8.8" -Count 1 -Quiet
+[int]$LimitsTryCounts = "0"
+
+# Scripts Blocks
+
+<#=============#>
+
+# [INIT]
+# Check Internet Connection
+if (-not ($Internet_NET)) {
+    Clear-Host
+        Write-Host <##>
+    Write-Host "    Erro de conexão" -ForegroundColor Red
+        Write-Host <##>
+
+        Start-Sleep -Seconds 1
+
+        Write-Host <##>
+    Write-Host "    Tentando novamente " -ForegroundColor Yellow -NoNewline
+    Write-Host "$LimitsTryCounts" -ForegroundColor Cyan
+        Write-Host <##>
+
+        do { <# Loop [INI] #>
+        # ENV
+        [bool]$Internet_NET = Test-Connection -TargetName "8.8.8.8" -Count 1 -Quiet
+        $LimitsTryCounts++
+
+        Clear-Host
+            Write-Host <##>
+        Write-Host "    Erro de conexão" -ForegroundColor Red
+            Write-Host <##>
+
+            Write-Host <##>
+        Write-Host "    Tentando novamente " -ForegroundColor Yellow -NoNewline
+        Write-Host "$LimitsTryCounts" -ForegroundColor Cyan
+            Write-Host <##>
+        } until (($Internet_NET -eq $true) -or ($LimitsTryCounts -gt 20))
+}

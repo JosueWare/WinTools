6ac14da633505b7bf3e5499ab5570ed698c4aa78
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-09-05 03:01:59 -0300

    feat

--- Config/Scripts/AccessBIOs.ps1
+++ Config/Scripts/AccessBIOs.ps1
@@ -5,3 +5,14 @@
     # Script Blocks
 
 # Begin
+Clear-Host
+Start-Sleep -Seconds 1
+
+    Write-Host "" <##>
+Write-Host " Aviso:" -ForegroundColor Yellow
+    Start-Sleep -Seconds 1
+Write-Host @(
+    "
+    A sua placa-mãe precisa ser compatível a esta função
+    "
+) -ForegroundColor Yellow

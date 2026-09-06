2ed722a254830dbd6e4a7d65da78e55a9022e0fb
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-09-05 21:56:32 -0300

    feat: Agora é só testar

--- Config/Scripts/AccessBIOs.ps1
+++ Config/Scripts/AccessBIOs.ps1
@@ -1,3 +1,11 @@
+# Logon
+
+    # Scripts Blocks
+    [scriptblock]$ExecBIOsAccess = {
+        shutdown.exe /r /fw /t 0
+    }
+
+# Begin
 Clear-Host
 Start-Sleep -Seconds 1
 
@@ -23,7 +31,11 @@ $questProssid = Read-Host
 
     switch ($questProssid) {
 
-        "S" {}
+        "S" {
+            Clear-Host
+            Start-Sleep -Seconds 2
+            & $ExecBIOsAccess
+        }
 
         "N" {& ".\Config\Main\Menu.ps1"}
 

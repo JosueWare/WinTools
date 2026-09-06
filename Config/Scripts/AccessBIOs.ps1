609cfbc240cef91d98413c023fb8458dac3cd9be
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-09-05 21:36:27 -0300

    feat

--- Config/Scripts/AccessBIOs.ps1
+++ Config/Scripts/AccessBIOs.ps1
@@ -1,10 +1,3 @@
-# Logon
-
-    # ENV
-
-    # Script Blocks
-
-# Begin
 Clear-Host
 Start-Sleep -Seconds 1
 
@@ -16,3 +9,23 @@ Write-Host @(
     A sua placa-mãe precisa ser compatível a esta função
     "
 ) -ForegroundColor Yellow
+
+    Start-Sleep -Seconds 1
+
+    Write-Host "" <##>
+Write-Host "    Deseja prosseguir? (Salve seus arquivos da área de trabalho!)" -ForegroundColor Yellow
+    Write-Host "" <##>
+    Start-Sleep -Milliseconds 500
+Write-Host "        [S] Sim | [N] Não"
+    Write-Host "" <##>
+
+$questProssid = Read-Host
+
+    switch ($questProssid) {
+
+        "S" {}
+
+        "N" {& ".\Config\Main\Menu.ps1"}
+
+        Default {& $ErrorResponse}
+    }

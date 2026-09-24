6abd0ef604e83f7413893eb24cc9985151010054
Josue Aguiar <josueaguiar.sc@gmail.com>
2026-09-24 03:36:08 -0300

    refactor

--- WinTools.ps1
+++ WinTools.ps1
@@ -1,17 +1,24 @@
-# Status de execução ADMIN
+# ADMIN Status
 $runnigAdmin = ([Security.Principal.WindowsPrincipal] `
     [Security.Principal.WindowsIdentity]::GetCurrent()
 ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-# Verificar se o 'WinTools' estiver sendo executado pelo PowerShell Core
-if ($PSVersionTable.PSEdition -eq "Core") {
-    if ($runnigAdmin) { <# Verificar se está sendo executado como administrador #>
-        & (Join-Path -Path $PSScriptRoot -ChildPath @("Config", "Main", "Menu.ps1"))
+# CheckOS
+if ($env:OS -eq "Windows_NT") {
+    # Check PS Edition
+    if ($PSVersionTable.PSEdition -eq "Core") {
+        #Check ADMIN
+        if ($runnigAdmin) {
+            & (Join-Path -Path $PSScriptRoot -ChildPath @("Config", "Main", "Menu.ps1"))
+        }
+            else {
+                Write-Host "WinTools requer que você execute como administrador" -ForegroundColor Yellow
+            }
     }
         else {
-            Write-Host "WinTools requer que você execute como administrador" -ForegroundColor Yellow
+            Write-Host "WinTools nao é suportado para o Windows PowerShell 5.1" -ForegroundColor Red
         }
 }
     else {
-        Write-Host "WinTools nao é suportado para o Windows PowerShell 5.1" -ForegroundColor Red
+        Write-Host "WinTools foi feito para usar no Windows" -ForegroundColor Red
     }
